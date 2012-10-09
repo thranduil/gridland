36dd24ad2d86fb8ea458bccd86301ac422c74361
Robert <robert.slak@gmail.com>
2012-10-09 11:01:33 +0200

    Extending Agent class

--- agents/thesis_agents/AExplorer.java
+++ agents/thesis_agents/AExplorer.java
@@ -1,5 +1,40 @@
 package thesis_agents;
 
-public class AExplorer {
+import org.grid.agent.Agent;
+import org.grid.protocol.Neighborhood;
+import org.grid.protocol.NewMessage.Direction;
+
+public class AExplorer extends Agent{
+
+	@Override
+	public void terminate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receive(int from, byte[] message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void state(int stamp, Neighborhood neighborhood,
+			Direction direction, boolean hasFlag) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
